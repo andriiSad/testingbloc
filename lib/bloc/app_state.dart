68869fa7e9149d1cabd930dcb5342fdbfe86111a
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-02-21 14:10:20 +0100

    step-6

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,60 +1,25 @@
+import 'dart:typed_data' show Uint8List;
 import 'package:flutter/foundation.dart' show immutable;
-import 'package:testingbloc_course/models.dart';
-import 'package:collection/collection.dart';
 
 @immutable
 class AppState {
   const AppState({
     required this.isLoading,
-    required this.loginError,
-    required this.loginHandle,
-    required this.fetchedNotes,
+    required this.data,
+    required this.error,
   });
-  final bool isLoading;
-  final LoginErrors? loginError;
-  final LoginHandle? loginHandle;
-  final Iterable<Note>? fetchedNotes;
-
   const AppState.empty()
       : isLoading = false,
-        loginError = null,
-        loginHandle = null,
-        fetchedNotes = null;
+        data = null,
+        error = null;
+  final bool isLoading;
+  final Uint8List? data;
+  final Object? error;
 
   @override
   String toString() => {
         'isLoading': isLoading,
-        'loginError': loginError,
-        'loginHandle': loginHandle,
-        'fetchedNotes': fetchedNotes,
+        'hasData': data != null,
+        'error': error,
       }.toString();
-
-  @override
-  bool operator ==(covariant AppState other) {
-    final otherPropertiesAreEqual = isLoading == other.isLoading &&
-        loginError == other.loginError &&
-        loginHandle == other.loginHandle;
-
-    if (fetchedNotes == null && other.fetchedNotes == null) {
-      return otherPropertiesAreEqual;
-    } else {
-      return otherPropertiesAreEqual &&
-          (fetchedNotes?.isEqualTo(other.fetchedNotes) ?? true);
-    }
-  }
-
-  @override
-  int get hashCode => Object.hash(
-        isLoading,
-        loginError,
-        loginHandle,
-        fetchedNotes,
-      );
-}
-
-extension UnorderedEquality on Object {
-  bool isEqualTo(other) => const DeepCollectionEquality.unordered().equals(
-        this,
-        other,
-      );
 }
